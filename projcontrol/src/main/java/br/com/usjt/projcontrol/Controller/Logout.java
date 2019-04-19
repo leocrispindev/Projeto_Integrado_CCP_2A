afcd108fd0eb6a2d869fb9a5e2d28ba637ce0554
Andrei <Andrei@Andrei-PC>
2019-04-19 01:49:48 -0300

    Logout

---
+++ projcontrol/src/main/java/br/com/usjt/projcontrol/Controller/Logout.java
@@ -0,0 +1,25 @@
+package br.com.usjt.projcontrol.Controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.usjt.projcontrol.Interface.Acao;
+
+public class Logout implements Acao {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+		HttpSession sessao = request.getSession();
+		sessao.removeAttribute("usuarioLogado");
+		sessao.invalidate();
+		
+		return "forward:index.jsp";
+	}
+
+}
